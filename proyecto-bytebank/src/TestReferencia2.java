ab6fd7f822e3732974faef7baba6b1c552218f15
Jefferson Murillo <development@eticsas.co>
2022-12-06 17:13:54 -0500

    Curso 04 OO implementando referencias - FULL

--- proyecto-bytebank/src/TestReferencia2.java
+++ proyecto-bytebank/src/TestReferencia2.java
@@ -4,8 +4,8 @@ public class TestReferencia2 {
 	public static void main(String[] args) {
 		Cliente cliente = new Cliente();
 		cliente.nombre = "Jefferson";
-		cliente.documeto = "1004.679.814";
-		cliente.telefono = "+57 320-696-9109";
+		cliente.documeto = "1005.679.230";
+		cliente.telefono = "+57 320-687-9109";
 		
 		Cuenta cuentaDeJefferson = new Cuenta();
 		cuentaDeJefferson.agencia = 1;
